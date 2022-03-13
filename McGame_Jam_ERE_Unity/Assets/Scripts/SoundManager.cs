036d8248a4cb98c9ba02c971508e2aab46c8074b
unknown <rowan.kerwin@mail.mcgill.ca>
2022-03-13 13:00:55 -0400

    working audio

--- McGame_Jam_ERE_Unity/Assets/Scripts/SoundManager.cs
+++ McGame_Jam_ERE_Unity/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+
+    public Rigidbody2D rbPlayer;
+    public AudioClip sound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        rbPlayer.gameObject.GetComponent<AudioSource>().PlayOneShot(sound, 1f);
+        Debug.Log(rbPlayer.gameObject.GetComponent<AudioSource>().clip);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!rbPlayer.gameObject.GetComponent<AudioSource>().isPlaying)
+        {
+            Debug.Log("p");
+            rbPlayer.gameObject.GetComponent<AudioSource>().Play();
+        }
     }
+
 }
